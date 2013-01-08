a7c486604a85081fe58b6905ff2cf6a4fca12dc3
Phil Stoll <philstoll@gmail.com>
2013-01-08 04:22:21 +0000

    Sign In and Out

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 SampleApp::Application.routes.draw do
   resources :users
-
-  root to: 'static_pages#home'
+  resources :sessions, only: [:new, :create, :destroy]
 
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+
+  root to: 'static_pages#home'
 
   get "static_pages/home"
   get "static_pages/help"
@@ -59,7 +62,7 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  # root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
